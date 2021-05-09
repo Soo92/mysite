5a444c0c1c6c49400ff8ec38082de3a5c3598a73
Soo92 <imleeseongsu@gmail.com>
2021-05-09 10:52:49 +0900

    폰트리사이징 1차 제한사항 고민필요

--- templates/static/main.js
+++ templates/static/main.js
@@ -2,7 +2,7 @@ let removeToast;
 back_optin_list="회벽, 마루.jpg/청색 대리석.jpg/갈색톤.jpg/검은색 목재.jpg/밝은 나무.jpg/밝은 벽돌 콘크리트.jpg/밝은 콘크리트.jpg/밝은 회색, 마루.jpg/분홍 벽지.jpg/블루 원단.jpg/살구벽지,마루.jpg/살색 원단.jpg/어두운 콘크리트.jpg/자홍색벽지.jpg/청색벽, 소나무 원목.jpg/한지 텍스쳐.jpg/흰벽,밝은마루.jpg/흰벽지,마루.jpg/흰색 나무결(2).jpg/흰색 나무결.jpg"
 font_list="A01/A02/A03/A04/A05/A06/A07/A08/A09/A10/B01/B02/B03/B04/B05/B06/B07/B08/B09/B10"
 
-var price_KE ={}
+price_KE ={}
 price_KE["4cm"]=1200
 price_KE["5cm"]=1500
 price_KE["6cm"]=2000
@@ -10,7 +10,7 @@ price_KE["8cm"]=3500
 price_KE["10cm"]=4500
 price_KE["15cm"]=7000
 price_KE["20cm"]=9500
-var price_ETC ={}
+price_ETC ={}
 price_ETC["4cm"]=2000
 price_ETC["5cm"]=2200
 price_ETC["6cm"]=3000
@@ -19,6 +19,10 @@ price_ETC["10cm"]=5000
 price_ETC["15cm"]=8000
 price_ETC["20cm"]=10000
 
+maxWH = {}
+maxWH["a"]=[900,800]
+maxWH["b"]=[550,550]
+maxWH["C"]=[900,600]
 
 document.fonts.ready.then(function () {
   preview_init();
@@ -43,7 +47,7 @@ function createRange(node, chars, range) {
                  chars.count = 0;
             }
         } else {
-            for (var lp = 0; lp < node.childNodes.length; lp++) {
+            for (lp = 0; lp < node.childNodes.length; lp++) {
                 range = createRange(node.childNodes[lp], chars, range);
                 if (chars.count === 0) {
                    break;
@@ -56,7 +60,7 @@ function createRange(node, chars, range) {
 
 function setCurrentCursorPosition(chars) {
     if (chars >= 0) {
-        var selection = window.getSelection();
+        selection = window.getSelection();
         range = createRange(document.getElementById("preview_text").parentNode, { count: chars });
         if (range) {
             range.collapse(false);
@@ -81,7 +85,7 @@ function toast(string) {
 }
 
 function showTextFile() {
-  var input = document.getElementById('ssa');
+  input = document.getElementById('ssa');
 
   const selectedFiles = input.files;
   const list = document.createElement('ul');
@@ -119,11 +123,6 @@ function first_enter(){
     tmp.innerText=""
   } else if (tmp.innerText.trim()=="") {
     tmp.spellcheck=true
-    if(f_wh=="A") {
-      tmp.innerHTML="여기에<br>써보세요"
-    } else if (f_wh=="B") {
-      tmp.innerHTML="WRITE<br>HERE"
-    }
   }
 }
 
@@ -233,25 +232,59 @@ function change_font(wh,e) {
   trg=tmp.siblings("a:visible");
   if(trg.length>0) {
     if (!trg.hasClass('clicked')) {
-      console.log("ddd")
       select_itm('#pro_font',trg.first());
+      console.log("dd")
     } else {
-      console.log("eee")
       trg=tmp.siblings("a.clicked:visible");
       select_itm('#pro_font',trg.first());
+      console.log("ddee")
     }
   }
 }
 
+function font_resize(){
+  sel_size = $("#pro_size");
+  f_s = sel_size.data("value")
+  f_h = f_s*5
+  $("#pre_text").children("p").each(function () {
+    $(this).text($('#preview_text').text());
+
+    aa=parseInt($(this).css('font-size'));  bb=$(this).height();
+    tmp_scale=f_h/bb;
+
+    $(this).data("scale",tmp_scale)
+    $(this).css('font-size',aa*tmp_scale+"px");
+
+    sub=$("#sub"+$(this).attr('id'));
+    sub.css('font-size',aa*tmp_scale+"px");
+  });
+}
+
 function preview_init(){
-  f_wh='A';
-  f_w=$('.dropdown > .dropbtn > span')
-  f_w.each(function () {
+  f_wh='A'; f_scale=1;
+
+  pro_w = document.getElementById('pro_wh').value;
+  pro_list=$('.for')
+  pro_list.each(function () {
+    if ($(this).attr('class').indexOf("hidden")<0) {
+      $(this).addClass("hidden")
+    }
+    tmp=$(this).attr('class').split(" ");
+    for (step = 0; step < tmp.length; step++) {
+      if (tmp[step].indexOf(pro_w)>-1 && $(this).attr('class').indexOf("hidden")>-1) {
+        $(this).removeClass("hidden")
+        break;
+      }
+    };
+  });
+
+  f_list=$('.dropdown > .dropbtn > span')
+  f_list.each(function () {
     init=$(this).text()
     m_h=$(this).parent().height();
     m_w=$(this).parent().width();
     tmp=$(this).parent().siblings('.dropdown-content').children('a');
-    for (var i = 0; i < tmp.length; i++) {
+    for (i = 0; i < tmp.length; i++) {
       if (tmp[i].innerText==$(this).data("select") && ($(tmp[i]).attr("class")==undefined||$(tmp[i]).attr("class").indexOf("clicked")<1)) {
         $(tmp[i]).addClass("clicked")
       }
@@ -264,49 +297,81 @@ function preview_init(){
     $(this).parent().width(m_w);
   });
 
-  pro_w = document.getElementById('pro_wh').value;
-  f_w=$('.for')
-  f_w.each(function () {
-    if ($(this).attr('class').indexOf("hidden")<0) {
-      $(this).addClass("hidden")
-    }
-    tmp=$(this).attr('class').split(" ");
-    for (step = 0; step < tmp.length; step++) {
-      if (tmp[step].indexOf(pro_w)>-1 && $(this).attr('class').indexOf("hidden")>-1) {
-        $(this).removeClass("hidden")
-        break;
-      }
-    };
-  });
-  aa = document.getElementById('back_dr');
-  element=back_optin_list.split("/")[0];
-  bb = document.getElementById('back_img');
-  bb.innerText=element.split(".")[0];
-  bb.setAttribute("data-value",element);
-  bb.setAttribute("data-select",element.split(".")[0]);
-  for (const element of back_optin_list.split("/")) {
-    var tmp = document.createElement('img');
-    tmp.src="static/background/"+element;
-    pre_img = document.getElementById('pre_img');
-    pre_img.appendChild(tmp);
 
-    var aac = document.createElement('a');
-    aac.innerText=element.split(".")[0];
-    aac.setAttribute("class","dropbtn");
-    aac.setAttribute("style","float:left;background-image:url('static/background/"+element+"');width:48px;height:30px;font-size:0px;");
-    aac.setAttribute("data-value",element);
-    aac.setAttribute("onclick","select_itm(back_img,this);preview_update()");
-    aa.appendChild(aac);
-  }
-  pre_text = document.getElementById('pre_text');
-  for (const element of font_list.split("/")) {
-    var opt = document.createElement('p');
-    opt.style = "font-family:"+element+";font-size:20px;height:auto;margin:0 auto;";
-    opt.innerText = element+":ㄱ가간agfG";
-    opt.id = "font"+element;
-    pre_text.appendChild(opt);
+  aa = document.getElementById('back_dr');
+  if($(aa).children().length==0) {
+    element=back_optin_list.split("/")[0];
+    bb = document.getElementById('back_img');
+    bb.innerText=element.split(".")[0];
+    bb.setAttribute("data-value",element);
+    bb.setAttribute("data-select",element.split(".")[0]);
+    for (const element of back_optin_list.split("/")) {
+      tmp = document.createElement('img');
+      tmp.src="static/background/"+element;
+      pre_img = document.getElementById('pre_img');
+      pre_img.appendChild(tmp);
+
+      aac = document.createElement('a');
+      aac.innerText=element.split(".")[0];
+      aac.setAttribute("class","dropbtn");
+      aac.setAttribute("style","float:left;background-image:url('static/background/"+element+"');width:48px;height:30px;font-size:0px;");
+      aac.setAttribute("data-value",element);
+      aac.setAttribute("onclick","select_itm(back_img,this);preview_update()");
+      aa.appendChild(aac);
+    }
+    pre1 = document.getElementById('pre_text');
+    pre2 = document.getElementById('pre_text2');
+    for (const element of font_list.split("/")) {
+      opt = document.createElement('p');
+      opt.id = "font"+element;
+      opt.style = "font-family:"+element+";font-size:20px;height:auto;width:fit-content;margin:0;";
+      opt.innerText = element+":ㄱ가간agfG";
+      opt.setAttribute("data-scale",1);
+      pre1.appendChild(opt);
+      opt = document.createElement('p');
+      opt.id = "subfont"+element;
+      opt.style = "font-family:"+element+";font-size:20px;height:auto;width:fit-content;margin:0;";
+      opt.innerText = element+":ㄱ가간agfG";
+      opt.setAttribute("data-scale",1);
+      pre2.appendChild(opt);
+    }
   }
 }
+
+function preview_check(a,event,aa){
+//  event.preventDefault();
+  prevK=event.which || event.keyCode;
+
+//  console.log(aa);
+//  console.log(prevK)
+//  console.log($(a).text())
+//  console.log(a.innerText)
+//  console.log($(a).html())
+//  console.log(a.innerHTML)
+//  console.log($(a).height()+"/"+$(a).width())
+
+  sel_font = $("#pro_font");
+  f_w = sel_font.data("value")
+  tmp=document.getElementById("font2"+f_w);
+//  console.log("font"+f_w)
+//  console.log(tmp.innerText)
+  if(a.innerText!="") {
+//    tmp.innerText=a.innerText;
+  }
+//  (f_h*f_scale*back_scale)
+
+  max_w = maxWH[pro_w][0]*back_scale/2
+  max_h = maxWH[pro_w][1]*back_scale/2
+
+//  console.log(back_scale+"/"+max_w)
+
+  if (a.clientHeight>max_h) {
+//    console.log(a.innerText.split("\n").length)
+  }
+  if (a.clientWidth>max_w) {
+//    console.log("d"+a.innerText.split("\n").length)
+  }
+};
 
 function preview_update(){
   tmp=document.getElementById("preview_text")
@@ -316,16 +381,16 @@ function preview_update(){
     } else if (f_wh=="B") {
       tmp.innerHTML="WRITE<br>HERE"
     }
+    font_resize();
   }
 
   pre_width=document.getElementById("pro_back").width
   pre_height=document.getElementById("pro_back").height
-
-  var sel_text_list = document.getElementById("preview_list")
-  var sel_text = document.getElementById("preview_text").innerText
-  var sel_HTML = document.getElementById("preview_text").innerHTML
+  sel_text_list = document.getElementById("preview_list")
+  sel_text = document.getElementById("preview_text").innerText
+  sel_HTML = document.getElementById("preview_text").innerHTML
   if (sel_text.length>20) {
-    toast("20자씩 나눠서 입력해 주세요!")
+    toast("20자씩 나눠서 입력해주세요!")
     sel_text=sel_text.slice(0,20)
     document.getElementById("preview_text").innerText=sel_text
     console.log(setCurrentCursorPosition())
@@ -347,33 +412,43 @@ function preview_update(){
     document.getElementById("preview_text").innerText=sel_text
   }
 
-  var sel_clr = $("#pro_color")
-  var sel_font = $("#pro_font");
-  var sel_size = $("#pro_size");
-  var sel_back = $('#back_img');
-  var sel_align = $('#pro_align');
-  var sel_deco = $('#side_deco');
-  var sel_main_LED = $("#pro_color_LED");
+  sel_clr = $("#pro_color");
+  sel_font = $("#pro_font");
+  sel_size = $("#pro_size");
+  sel_back = $('#back_img');
+  sel_align = $('#pro_align');
+  sel_deco = $('#side_deco');
+  sel_main_LED = $("#pro_color_LED");
 
-  var t_clr = sel_clr.data("select")
-  var t_main_LED = sel_main_LED.data("select")
-  var t_side_LED = $("#side_color_on").data("select");
-  var t_side_back = $("#side_color_off").data("select");
-  var t_font = sel_font.data("select")
-  var t_size = sel_size.data("select")
-  var t_align = sel_align.data("select")
-  var t_deco = sel_deco.data("select")
+  t_clr = sel_clr.data("select")
+  t_main_LED = sel_main_LED.data("select")
+  t_side_LED = $("#side_color_on").data("select");
+  t_side_back = $("#side_color_off").data("select");
+  t_font = sel_font.data("select")
+  t_size = sel_size.data("select")
+  t_align = sel_align.data("select")
+  t_deco = sel_deco.data("select")
 
-  var f_b = sel_back.data("value")
-  var f_w = sel_font.data("value")
-  var f_h = sel_size.data("value")
-  var f_align = sel_align.data("value")
-  var f_deco = sel_deco.data("value");
+  f_b = sel_back.data("value")
+  f_w = sel_font.data("value")
+  f_s = sel_size.data("value")
+  f_h = f_s*5
+  f_align = sel_align.data("value")
+  f_deco = sel_deco.data("value");
 
-  var f_light = $("#pro_light").text().indexOf("OFF")>0
-  var f_main_LED = $("#pro_color_LED").data("value")
-  var f_side_LED = $("#side_color_on").data("value");
-  var f_side_back = $("#side_color_off").data("value");
+  if (f_s<8) {    back_scale=1.4
+  } else if (f_s<15) {  back_scale=1.2
+  } else {    back_scale=1  }
+
+  f_scale=$("#font"+f_w).data("scale")
+  console.log(f_scale)
+
+  font_size=(f_h*f_scale*back_scale)
+
+  f_light = $("#pro_light").text().indexOf("OFF")>0
+  f_main_LED = $("#pro_color_LED").data("value")
+  f_side_LED = $("#side_color_on").data("value");
+  f_side_back = $("#side_color_off").data("value");
 
   if (f_side_LED =="none") {
     f_side = false
@@ -403,25 +478,13 @@ function preview_update(){
   }
 
   if (pro_w=="a") {
-    var f_thi = 2
+    f_thi = 2
   } else if (pro_w="b") {
-    var f_thi = 10
+    f_thi = 10
   } else if (pro_w="c") {
 
   }
-  var f_cnt = 20
-
-  var max_w = 450
-  var max_h = 400
-
-  if (f_h<50) {    back_scale=1.5
-  } else {    back_scale=1  }
-
-  f_scale=1
-  tmp=document.getElementById("font"+f_w);
-//  tmp.innerText=sel_text.replace(/(\n|\r\n)/g, '');
-  aa=tmp.style.fontSize;  bb=tmp.clientHeight;
-  f_scale=parseInt(aa)/bb
+  f_cnt = 20
 
   if(f_light) {
     pre_back="background: linear-gradient(to top, rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.25)), url('static/background/"+f_b+"');"
@@ -449,8 +512,8 @@ function preview_update(){
     pre_style="";f_shwd="";
 
     if(step==0) {
-      f_clr=arr_clr[0]
       tmp=document.getElementById("preview_text")
+      f_clr=arr_clr[0]
       if (pro_w=="a") {
         pre_style="background:radial-gradient(farthest-corner at 40% -5%, #"+arr_clr[0]+" 3%, #"+arr_clr[1]+" 5%, #"+arr_clr[2]+" 35%, #"
                   +arr_clr[2]+" 65%, #"+arr_clr[1]+" 99%, #"+arr_clr[0]+" 1000%);"
@@ -464,6 +527,15 @@ function preview_update(){
           f_shwd=f_shwd+";"
         }
     } else {
+      tmpid="side"+(step);
+      tmp=document.getElementById(tmpid)
+      if(!tmp) {
+        tmp = document.createElement('div');
+        tmp.id=tmpid
+        tmp.className="drcnt preview_3d_text";
+        sel_text_list.appendChild(tmp);
+      }
+      tmp.innerText=sel_text;
       if (pro_w=="a") {
         if (step==1) {
           f_clr=arr_clr[1]
@@ -473,7 +545,6 @@ function preview_update(){
       } else if (pro_w="b") {
         if (step<=(f_cnt*15/20)) {
           taper=taper+(1/3)
-          // /*f_h/100
         }
 
         f_shwd="text-shadow:"
@@ -497,15 +568,6 @@ function preview_update(){
         }
         f_shwd=f_shwd+taper+"px "+taper+"px red;"
       }
-      tmpid="side"+(step);
-      tmp=document.getElementById(tmpid)
-      if(!tmp) {
-        tmp = document.createElement('div');
-        tmp.id=tmpid
-        tmp.className="drcnt preview_3d_text";
-        sel_text_list.appendChild(tmp);
-      }
-      tmp.innerText=sel_text;
     }
 //6 10 35 60
     if (step == f_cnt) {
@@ -515,8 +577,8 @@ function preview_update(){
         f_thi*3/4+"px "+0.5*f_thi*3/4+"px "+f_thi*1.4+"px rgba(16,16,16,0.2),"+
         f_thi*4/4+"px "+0.5*f_thi*4/4+"px "+f_thi*2+"px rgba(16,16,16,0.4);"
     }
-    pre_style=pre_style+"width:fit-content;min-width:"+f_h+"px;min-height:"+f_h+"px;max-width:"+max_w+"px;max-height:"+max_h+"px;"
-      +""+f_shwd+"color:#"+ f_clr +";font-family:"+f_w+";font-size:"+(f_h*f_scale*back_scale)+"px;text-align:"+f_align+";"
+    pre_style=pre_style+"width:fit-content;height:fit-content;min-width:"+f_h+"px;min-height:"+f_h+"px;"
+      +""+f_shwd+"color:#"+ f_clr +";font-family:"+f_w+";font-size:"+font_size+"px;text-align:"+f_align+";"
       +"z-index:"+(f_cnt-step)+";left:"+(pre_width/2+step*0.5*f_thi/f_cnt)+"px;top:"+(pre_height/2+step*f_thi/f_cnt)+"px;word-wrap:break-word;position:absolute;"
     if (f_deco=="underL") {
       pre_style=pre_style+"text-decoration: underline;"
@@ -527,14 +589,14 @@ function preview_update(){
   }
 
 /*외부 수정 가능하게*/
-  var price_total = 0
+  price_total = 0
 
-  var check_num = /[0-9]/;
-  var check_eng = /[a-zA-Z]/;
-  var check_spc = /[~!@#$%^&*()_+|<>?:{}]/;
-  var check_kor = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
-  var check_n = /\n/;
-  var check_s = /\s/;
+  check_num = /[0-9]/;
+  check_eng = /[a-zA-Z]/;
+  check_spc = /[~!@#$%^&*()_+|<>?:{}]/;
+  check_kor = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+  check_n = /\n/;
+  check_s = /\s/;
 
   o_cnt=0;e_cnt=0;k_cnt=0;f_cnt=0;
   n_cnt=0;s_cnt=0;
