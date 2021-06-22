d38406e260e07428abdeb4bfe310a1de60a9d5b1
Soo92 <imleeseongsu@gmail.com>
2021-06-22 23:52:14 +0900

    asf12

--- flask_app.py
+++ flask_app.py
@@ -3,13 +3,17 @@
 
 from flask import Flask,render_template
 import pandas as pd
-import sys
+import sys,os
 sys.path.append("templates/static")
-sys.path.append("/home/SeongSuLee/mysite")
 sys.path.append("/home/SeongSuLee/mysite/templates/static")
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))+"/templates/static"
+print(THIS_FOLDER)
+MY_FILE = os.path.join(MY_DIR,"keyw.csv")
+print(MY_FILE)
+
 for p in sys.path:
     print( p )
-# import f_key as ff
+import f_key as ff
 
 app = Flask(__name__,static_url_path='/templates/static/')
 
@@ -31,14 +35,14 @@ def t5():
 
 @app.route('/admin')
 def admin():
-    html_all = pd.read_csv("templates/static/keyw.csv", engine='python').to_html(table_id = "csv_table",classes="display compact")
+    html_all = pd.read_csv(MY_FILE, engine='python').to_html(table_id = "csv_table",classes="display compact")
     html_add = pd.read_csv("templates/static/my.csv", engine='python').to_html(table_id = "add_table",classes="display compact")
     html_del = pd.read_csv("templates/static/del.csv", engine='python').to_html(table_id = "del_table",classes="display compact")
     return render_template('ad_1.html',data_key=html_all,data_add=html_add,data_del=html_del)
 
 @app.route('/reload')
 def reload():
-    # ff.reload()
+    ff.reload()
     return "hello"
 
 if __name__ == '__main__':
