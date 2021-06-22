f572465465619832de08bdcb11abbfe61dd784b2
Soo92 <imleeseongsu@gmail.com>
2021-06-23 00:01:58 +0900

    asd12fg

--- flask_app.py
+++ flask_app.py
@@ -7,9 +7,6 @@ import sys,os
 sys.path.append("templates/static")
 sys.path.append("/home/SeongSuLee/mysite/templates/static")
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))+"/templates/static"
-print(THIS_FOLDER)
-MY_FILE = os.path.join(THIS_FOLDER,"keyw.csv")
-print(MY_FILE)
 
 for p in sys.path:
     print( p )
@@ -35,9 +32,12 @@ def t5():
 
 @app.route('/admin')
 def admin():
-    html_all = pd.read_csv(MY_FILE, engine='python').to_html(table_id = "csv_table",classes="display compact")
-    html_add = pd.read_csv("templates/static/my.csv", engine='python').to_html(table_id = "add_table",classes="display compact")
-    html_del = pd.read_csv("templates/static/del.csv", engine='python').to_html(table_id = "del_table",classes="display compact")
+    FILE1 = os.path.join(THIS_FOLDER,"keyw.csv")
+    FILE2 = os.path.join(THIS_FOLDER,"my.csv")
+    FILE3 = os.path.join(THIS_FOLDER,"my.csv")
+    html_all = pd.read_csv(FILE1, engine='python').to_html(table_id = "csv_table",classes="display compact")
+    html_add = pd.read_csv(FILE2, engine='python').to_html(table_id = "add_table",classes="display compact")
+    html_del = pd.read_csv(FILE3, engine='python').to_html(table_id = "del_table",classes="display compact")
     return render_template('ad_1.html',data_key=html_all,data_add=html_add,data_del=html_del)
 
 @app.route('/reload')
